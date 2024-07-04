54a3a1dcaf306116d1d5ac7659052610ff3e1f20
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2024-07-04 00:40:38 -0400

    Demonstrate how to print errors

--- examples/read_doc_example.dart
+++ examples/read_doc_example.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 import 'package:boomsheets_dart/lib.dart';
+import 'package:yes_parser/yes_parser.dart';
 
 void main() async {
   Document doc = await DocumentReader.fromFile(
     File.fromUri(
       Uri.parse("examples/test.anim"),
     ),
+    onErrors: printErrors,
   );
 
   for (final MapEntry(:key, :value) in doc.states.entries) {
@@ -19,5 +21,14 @@ void main() async {
       }
       print(keyframe);
     }
+  }
+}
+
+void printErrors(List<ErrorInfo> errors) {
+  for (ErrorInfo info in errors) {
+    // We are not interested in empty line errors
+    if (info.type == ErrorType.eolNoData) continue;
+
+    print(">>>> Error on line ${info.line}: ${info.message}");
   }
 }
